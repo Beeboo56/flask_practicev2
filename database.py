3e9777a7f31799bd1b4ba391e5622148b703591c
Beeboo56 <Arnaudverstraeten01@gmail.com>
2025-04-26 01:30:38 +0000

    store application in database

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-import sqlalchemy
 from sqlalchemy import create_engine, text
 import os
 
@@ -24,5 +23,33 @@ def load_job_from_db(id):
       return None
     else:
       return dict(rows[0])
-  
+
+def add_application_to_db(job_id, data):
+  with engine.begin() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    
+    #conn.execute(
+        #query,
+        #job_id=job_id,
+        #full_name=data['full_name'],
+        #email=data['email'],
+        #linkedin_url=data['linkedin_url'],
+        #education=data['education'],
+        #work_experience=data['work_experience'],
+        #resume_url=data['resume_url']
+    #)
+
+    conn.execute(
+        query,
+        {
+            "job_id": job_id,
+            "full_name": data['full_name'],
+            "email": data['email'],
+            "linkedin_url": data['linkedin_url'],
+            "education": data['education'],
+            "work_experience": data['work_experience'],
+            "resume_url": data['resume_url']
+        }
+    )
+      
   
